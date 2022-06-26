bcb414383b37be8373a43f94c3b171f03570dab7
xariasavila <xaviera.arias.a@gmail.com>
2022-06-25 23:40:56 -0400

    index a root

--- TareaSemana6/js/tarea6_pokemon.js
+++ TareaSemana6/js/tarea6_pokemon.js
@@ -1,38 +1,37 @@
 const getPokemon = async () => {
   try {
-    const request = await axios("https://pokeapi.co/api/v2/pokemon/"); 
+    const request = await axios("https://pokeapi.co/api/v2/pokemon/");
     const pokemonData = request.data;
     pokemonData.results.forEach(function (pokemon, index) {
       mostrarPokemon(pokemon, index);
     });
 
-      
     function mostrarPokemon(pokemon, index) {
       const pokemonCard = document.createElement("pokemonCard");
       pokemonCard.innerHTML = `<pokemonCard>
       <div class="card" style="width: 18rem;">
       <img class="card-img-top" 
-      src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png'
+      src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        index + 1
+      }.png'
       alt="Card image cap">
       <div class="card-body">
         <p class="card-text">${pokemon.name.toUpperCase()}</p>
       </div>
               </div>
               </pokemonCard>
-              `
+              `;
 
-       document.body.appendChild(pokemonCard);
+      document.body.appendChild(pokemonCard);
     }
-
   } catch (error) {
-    
-      error.innerHTML = `<error>
+    error.innerHTML = `<error>
     <p> No se pudo cargar la data  </p>
             </error>
-            `
+            `;
 
-     document.body.appendChild(error);
-      
+    document.body.appendChild(error);
+
     console.log(error);
   } finally {
     console.log("cargado correctamente");
